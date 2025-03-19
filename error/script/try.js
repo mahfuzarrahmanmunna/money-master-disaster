56a299869a1c8f3826e6703f2300cc57a7061ddd
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-03-20 04:48:22 +0600

    alhamdulillah motamuti hoiche

---
+++ error/script/try.js
@@ -0,0 +1,14 @@
+
+try {
+    // code likhbo
+    console.log('ami crieket khelbo')
+    console.log(num)
+    console.log('ok')
+    console.log('ami football khelbo')
+
+}
+catch (err) {
+    console.error(err)
+} finally {
+    console.log('finally ami football khelbo0011')
+}
